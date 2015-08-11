f19afb6bf00485aff34d7fe87c2cec4f7b676d0b
Manfred Touron <m@42.am>
2015-08-12 01:52:47 +0200

    App/Session/Login stuffs are working

--- cmd/freebox/main.go
+++ cmd/freebox/main.go
@@ -14,8 +14,22 @@ func main() {
 
 	err := fbx.Connect()
 	if err != nil {
-		panic(err)
+		logrus.Fatalf("fbx.Connect(): %v", err)
 	}
 
-	fmt.Println(fbx.DownloadsStats())
+	err = fbx.Authorize()
+	if err != nil {
+		logrus.Fatalf("fbx.Authorize(): %v", err)
+	}
+
+	err = fbx.Login()
+	if err != nil {
+		logrus.Fatalf("fbx.Login(): %v", err)
+	}
+
+	stats, err := fbx.DownloadsStats()
+	if err != nil {
+		logrus.Fatalf("fbx.DownloadsStats(): %v", err)
+	}
+	fmt.Println(stats)
 }
